72976449915eae58afc7c31b15f76bd76bfd437a
Yulin Zhang <ylzhang0008@gmail.com>
2022-08-07 21:51:53 -0500

    add zipper merge

---
+++ exp_shell/train/zipper_simple_merge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FLOW_DIR=${PWD}/../..
+export PYTHONPATH="${PYTHONPATH}:${FLOW_DIR}"
+export RAY_MEMORY_MONITOR_ERROR_THRESHOLD=0.8
+
+
+# Even vehicle placement
+MERGE_INFLOW=200
+
+
+let MAIN_RL_INFLOW=0
+let MAIN_HUMAN_INFLOW=MAIN_INFLOW
+echo "Avp:${AVP}, Inflows:${MAIN_HUMAN_INFLOW} ${MAIN_RL_INFLOW} ${MERGE_INFLOW}"
+python3 ${FLOW_DIR}/examples/rllib/multiagent_exps/zipper_simple_merge_collaborate.py \
+    --handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
+    --exp_folder_mark Even_Avp${AVP}_Main${MAIN_INFLOW}_Merge${MERGE_INFLOW}
+
+
