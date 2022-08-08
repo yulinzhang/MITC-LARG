f56ea9e758da71a799037948ec069355a0879bfa
Yulin Zhang <ylzhang0008@gmail.com>
2022-08-07 21:51:50 -0500

    update shell

--- exp_shell/evaluate/zipper_simple_merge.sh
+++ exp_shell/evaluate/zipper_simple_merge.sh
@@ -9,10 +9,10 @@ single_lane_simple_merge_shadow=${HOME}/aug3/PPO_MultiAgentMerge4ShadowHeadwayPO
 
 
 FLOW_DIR=${PWD}/../..
-VISUALIZER=$FLOW_DIR/flow/visualize/new_rllib_visualizer.py
-#VISUALIZER=$FLOW_DIR/flow/visualize/parallized_visualizer.py
+#VISUALIZER=$FLOW_DIR/flow/visualize/new_rllib_visualizer.py
+VISUALIZER=$FLOW_DIR/flow/visualize/parallized_visualizer.py
 EXP_FOLDER=$FLOW_DIR/exp_results
-WORKING_DIR=$EXP_FOLDER/aug4_simple_merge_shadow
+WORKING_DIR=$EXP_FOLDER/aug7_zipper_simple_merge
 
 # 1. 1650_200_30 I=4
 # 2. 1850_200_30 I=5
@@ -34,7 +34,7 @@ MAIN_RL=0
 MERGE=200
 measurement=8000
 WINDOW=400
-render=sumo_gui
+render=no_render
 
 CHCKPOINT=1
 
@@ -44,22 +44,22 @@ CHCKPOINT=1
 mkdir ${WORKING_DIR}
 J=0
 
-for MAIN_HUMAN in 2000 #6000 8000 10000 #400 600 800 
+for MAIN_HUMAN in 2000 1800 1600 1400 1200 #6000 8000 10000 #400 600 800 
 do
-    #python3 $VISUALIZER \
-    #            ${zipper_simple_merge} \
-    #            $CHCKPOINT \
-    #            --seed_dir $FLOW_DIR \
-    #            --horizon 4000 \
-    #            --render_mode ${render} \
-    #            --num_of_rand_seeds 50 \
-    #            --cpu 52 \
-    #            --measurement_rate ${measurement} \
-    #            --to_probability \
-    #            --max_deceleration 20 \
-    #            --handset_inflow $MAIN_HUMAN $MAIN_RL $MERGE \
-    #            --window_size $WINDOW $WINDOW $WINDOW 
-    #            >> ${WORKING_DIR}/EVAL_IDM_${MAIN_HUMAN}_${MERGE}.txt 
+    python3 $VISUALIZER \
+                ${zipper_simple_merge} \
+                $CHCKPOINT \
+                --seed_dir $FLOW_DIR \
+                --horizon 4000 \
+                --render_mode ${render} \
+                --num_of_rand_seeds 50 \
+                --cpu 52 \
+                --measurement_rate ${measurement} \
+                --to_probability \
+                --max_deceleration 20 \
+                --handset_inflow $MAIN_HUMAN $MAIN_RL $MERGE \
+                --window_size $WINDOW $WINDOW $WINDOW \
+                >> ${WORKING_DIR}/EVAL_IDM_${MAIN_HUMAN}_${MERGE}.txt 
 
     python3 $VISUALIZER \
                 ${zipper_simple_merge} \
@@ -74,8 +74,8 @@ do
                 --to_probability \
                 --max_deceleration 20 \
                 --handset_inflow $MAIN_HUMAN $MAIN_RL $MERGE \
-                --window_size $WINDOW $WINDOW $WINDOW 
-                #>> ${WORKING_DIR}/EVAL_shadow_window_${WINDOW}_${MAIN_HUMAN}_${MERGE}.txt 
+                --window_size $WINDOW $WINDOW $WINDOW \
+                >> ${WORKING_DIR}/EVAL_shadow_window_${WINDOW}_${MAIN_HUMAN}_${MERGE}.txt 
                 #--print_metric_per_time_step_in_file metrics 
                 #--merge2 ${MERGE2} \
                 #--agent_action_policy_dir ${TRAIN_DIR_shadow} \
