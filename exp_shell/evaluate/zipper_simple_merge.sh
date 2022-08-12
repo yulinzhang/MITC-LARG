3636cad856455c9458e78f9100283df50749fa71
Yulin Zhang <ylzhang0008@gmail.com>
2022-08-11 22:43:32 -0500

    update

--- exp_shell/evaluate/zipper_simple_merge.sh
+++ exp_shell/evaluate/zipper_simple_merge.sh
@@ -6,23 +6,23 @@ declare -A MARK
 
 zipper_simple_merge_idm=${HOME}/aug7/zipper_simple_merge_Even_Avp_Main_Merge200_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_10544_00000_0_2022-08-07_21-16-08
 zipper_simple_merge_shadow=${HOME}/aug7/zipper_simple_merge_shadow_Main_Merge200_Collaborate_lr_schedule_eta1_0.9_eta2_0.1/PPO_MultiAgentMerge4ShadowHeadwayPOEnvParameterizedWindowSizeCollaborate-v0_3ac32_00000_0_2022-08-07_22-50-22
-zipper_simple_merge_aamas="Please enter the path to the aamas model"
+zipper_simple_merge_aamas=${HOME}/aug10/zipper_simple_merge_aamas_Main2000_Merge200_AVP30/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_0e8b3_00000_0_2022-08-10_14-53-08
+priority_simple_merge_aamas=${HOME}/aug10/priority_simple_merge_Main2000_Merge200_AVP30/PPO_MultiAgentHighwayPOEnvMerge4Collaborate-v0_50fc3_00000_0_2022-08-10_16-49-31
 
 FLOW_DIR=${PWD}/../..
 #VISUALIZER=$FLOW_DIR/flow/visualize/new_rllib_visualizer.py
 VISUALIZER=$FLOW_DIR/flow/visualize/parallized_visualizer.py
 EXP_FOLDER=$FLOW_DIR/exp_results
-WORKING_DIR=$EXP_FOLDER/aug7_zipper_simple_merge
+WORKING_DIR=$EXP_FOLDER/aug11_zipper_simple_merge
 
 echo "*************add python path to current direction***********"
 export PYTHONPATH="${PYTHONPATH}:$FLOW_DIR"
 echo "set python path: $PYTHONPATH"
 echo "************************************************************"
 
-MERGE_INFLOW=200
 measurement=1000
 
-MERGE=200
+MERGE_INFLOW=200
 WINDOW=400
 render=no_render
 
@@ -31,26 +31,27 @@ CHCKPOINT=500
 mkdir ${WORKING_DIR}
 J=0
 for MAIN_INFLOW in 2000 1800 1600 1400 1200 #6000 8000 10000 #400 600 800 
-    let MAIN_RL=0
-    let MAIN_HUMAN=MAIN_INFLOW
+do
+    let MAIN_RL_INFLOW=0
+    let MAIN_HUMAN_INFLOW=MAIN_INFLOW
     # human baseline
-    python3 $VISUALIZER \
-        ${zipper_simple_merge_aamas} \
-        $CHCKPOINT \
-        --seed_dir $FLOW_DIR \
-        --horizon 4000 \
-        --render_mode ${render} \
-        --num_of_rand_seeds 50 \
-        --cpu 52 \
-        --measurement_rate ${measurement} \
-        --to_probability \
-        --max_deceleration 20 \
-        --handset_inflow $MAIN_HUMAN $MAIN_RL $MERGE \
-        --window_size $WINDOW $WINDOW $WINDOW 
-        >> ${WORKING_DIR}/EVAL_IDM_${MAIN_HUMAN}_${MERGE}.txt 
+    #python3 $VISUALIZER \
+    #    ${zipper_simple_merge_aamas} \
+    #    $CHCKPOINT \
+    #    --seed_dir $FLOW_DIR \
+    #    --horizon 4000 \
+    #    --render_mode ${render} \
+    #    --num_of_rand_seeds 50 \
+    #    --cpu 52 \
+    #    --measurement_rate ${measurement} \
+    #    --to_probability \
+    #    --max_deceleration 20 \
+    #    --handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
+    #    --window_size $WINDOW $WINDOW $WINDOW 
+    #    >> ${WORKING_DIR}/EVAL_IDM_${MAIN_HUMAN}_${MERGE}.txt 
 
     # aamas policy 
-    for AVP in 100
+    for AVP in 100 30
     do
         let MAIN_RL_INFLOW=MAIN_INFLOW*${AVP}/100
         let MAIN_HUMAN_INFLOW=MAIN_INFLOW-MAIN_RL_INFLOW
@@ -67,9 +68,9 @@ for MAIN_INFLOW in 2000 1800 1600 1400 1200 #6000 8000 10000 #400 600 800
                     --measurement_rate ${measurement} \
                     --to_probability \
                     --max_deceleration 20 \
-                    --handset_inflow $MAIN_HUMAN $MAIN_RL $MERGE \
+                    --handset_inflow $MAIN_HUMAN_INFLOW $MAIN_RL_INFLOW $MERGE_INFLOW \
                     --window_size $WINDOW $WINDOW $WINDOW \
-                    >> ${WORKING_DIR}/EVAL_shadow_window_${WINDOW}_${MAIN_HUMAN}_${MERGE}.txt 
+                    >> ${WORKING_DIR}/EVAL_zipper_aamas_${MAIN_HUMAN_INFLOW}_${MAIN_RL_INFLOW}_${MERGE_INFLOW}.txt 
                     #--print_metric_per_time_step_in_file metrics 
                     #--merge2 ${MERGE2} \
                     #--agent_action_policy_dir ${TRAIN_DIR_shadow} \
